cb05eef01ee8fa8b9f7f9a32d33734b870ac18c2
rotemn2006 <rotemneugeboren@hilma.tech>
2024-10-30 17:00:32 +0200

    static keyboard

--- src/App1.jsx
+++ src/App1.jsx
@@ -1,18 +1,77 @@
-import React from "react";
-import React, { useState } from 'react';
-const enKeyboard=["a","b","C","d","e","f","g","h","i","j","k","l","m","o","g","q","r","s","t","u","v","w","x","y","z","/","!","?"];
+//import React from "react";
+import React, { useState } from "react";
+import Keyboard from "./components/Keyboard";
+const enKeyboard = [
+  "a",
+  "b",
+  "C",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "o",
+  "g",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "/",
+  "!",
+  "?",
+];
 
-const hekeyboard=["א","ב","ג","ד","ה","ו","ז","ח","ט","י","כ","ל","מ","נ","ס","ע","פ","צ","ק","ר","ש","ת","/","!","?"];
+const hekeyboard = [
+  "א",
+  "ב",
+  "ג",
+  "ד",
+  "ה",
+  "ו",
+  "ז",
+  "ח",
+  "ט",
+  "י",
+  "כ",
+  "ל",
+  "מ",
+  "נ",
+  "ס",
+  "ע",
+  "פ",
+  "צ",
+  "ק",
+  "ר",
+  "ש",
+  "ת",
+  "/",
+  "!",
+  "?",
+];
 
-function App1(){
-    const [language, setLanguage]=useState(enKeyboard);
+function App1() {
+  // const [language, setLanguage]=useState(enKeyboard);
 
-    return(
-        <>
-        <h1>our keyboard</h1>
-        <div id=" keyboard">
-        </div>
-        </>
-    )
-
+  return (
+    <>
+      <h1>our keyboard!</h1>
+      <div id=" keyboard">
+        {enKeyboard.map((item, i) => (
+          <Keyboard id={i + 1} key={`CharNum${i + 1}`} thisChar={item} />
+        ))}
+      </div>
+    </>
+  );
 }
+export default App1;
---
+++ src/components/Keyboard.jsx
@@ -0,0 +1,4 @@
+function Keyboard(props) {
+  return <button>{props.thisChar}</button>;
+}
+export default Keyboard;
